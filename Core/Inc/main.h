aec57994d84777d4bb0ff94aceed4a9d5ed90182
TobiasNetzer <netzerto@gmail.com>
2025-10-14 21:17:43 +0200

    initial project config

---
+++ Core/Inc/main.h
@@ -0,0 +1,111 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define EXT_PROBE_ON_Pin GPIO_PIN_0
+#define EXT_PROBE_ON_GPIO_Port GPIOA
+#define EXT_TRIG_INT_Pin GPIO_PIN_0
+#define EXT_TRIG_INT_GPIO_Port GPIOB
+#define SHIFT_REG_G_STIM_Pin GPIO_PIN_1
+#define SHIFT_REG_G_STIM_GPIO_Port GPIOB
+#define SHIFT_REG_CLR_Pin GPIO_PIN_2
+#define SHIFT_REG_CLR_GPIO_Port GPIOB
+#define SHIFT_REG_EN_MEAS_H_Pin GPIO_PIN_10
+#define SHIFT_REG_EN_MEAS_H_GPIO_Port GPIOB
+#define SHIFT_REG_EN_MEAS_L_Pin GPIO_PIN_11
+#define SHIFT_REG_EN_MEAS_L_GPIO_Port GPIOB
+#define SHIFT_REG_EN_STIM_Pin GPIO_PIN_12
+#define SHIFT_REG_EN_STIM_GPIO_Port GPIOB
+#define SHIFT_REG_G_MEAS_H_Pin GPIO_PIN_13
+#define SHIFT_REG_G_MEAS_H_GPIO_Port GPIOB
+#define SHIFT_REG_G_MEAS_L_Pin GPIO_PIN_14
+#define SHIFT_REG_G_MEAS_L_GPIO_Port GPIOB
+#define PROBE_BTN_INT_Pin GPIO_PIN_6
+#define PROBE_BTN_INT_GPIO_Port GPIOC
+#define EXT_TRIG_ON_Pin GPIO_PIN_15
+#define EXT_TRIG_ON_GPIO_Port GPIOA
+#define EXT_STIM_1_ON_Pin GPIO_PIN_0
+#define EXT_STIM_1_ON_GPIO_Port GPIOD
+#define EXT_STIM_2_ON_Pin GPIO_PIN_1
+#define EXT_STIM_2_ON_GPIO_Port GPIOD
+#define EXT_STIM_3_ON_Pin GPIO_PIN_2
+#define EXT_STIM_3_ON_GPIO_Port GPIOD
+#define EXT_STIM_4_ON_Pin GPIO_PIN_3
+#define EXT_STIM_4_ON_GPIO_Port GPIOD
+#define SHIFT_REG_SRCK_Pin GPIO_PIN_3
+#define SHIFT_REG_SRCK_GPIO_Port GPIOB
+#define SHIFT_REG_SER_IN_Pin GPIO_PIN_5
+#define SHIFT_REG_SER_IN_GPIO_Port GPIOB
+#define SHIFT_REG_RCK_MEAS_H_Pin GPIO_PIN_6
+#define SHIFT_REG_RCK_MEAS_H_GPIO_Port GPIOB
+#define SHIFT_REG_RCK_MEAS_L_Pin GPIO_PIN_7
+#define SHIFT_REG_RCK_MEAS_L_GPIO_Port GPIOB
+#define SHIFT_REG_RCK_STIM_Pin GPIO_PIN_8
+#define SHIFT_REG_RCK_STIM_GPIO_Port GPIOB
+#define SHIFT_REG_ERR_Pin GPIO_PIN_9
+#define SHIFT_REG_ERR_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Core/Src/gpio.c
@@ -0,0 +1,112 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, EXT_PROBE_ON_Pin|EXT_TRIG_ON_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, SHIFT_REG_G_STIM_Pin|SHIFT_REG_CLR_Pin|SHIFT_REG_EN_MEAS_H_Pin|SHIFT_REG_EN_MEAS_L_Pin
+                          |SHIFT_REG_EN_STIM_Pin|SHIFT_REG_G_MEAS_H_Pin|SHIFT_REG_G_MEAS_L_Pin|SHIFT_REG_RCK_MEAS_H_Pin
+                          |SHIFT_REG_RCK_MEAS_L_Pin|SHIFT_REG_RCK_STIM_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, EXT_STIM_1_ON_Pin|EXT_STIM_2_ON_Pin|EXT_STIM_3_ON_Pin|EXT_STIM_4_ON_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : EXT_PROBE_ON_Pin EXT_TRIG_ON_Pin */
+  GPIO_InitStruct.Pin = EXT_PROBE_ON_Pin|EXT_TRIG_ON_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : EXT_TRIG_INT_Pin */
+  GPIO_InitStruct.Pin = EXT_TRIG_INT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(EXT_TRIG_INT_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SHIFT_REG_G_STIM_Pin SHIFT_REG_CLR_Pin SHIFT_REG_EN_MEAS_H_Pin SHIFT_REG_EN_MEAS_L_Pin
+                           SHIFT_REG_EN_STIM_Pin SHIFT_REG_G_MEAS_H_Pin SHIFT_REG_G_MEAS_L_Pin SHIFT_REG_RCK_MEAS_H_Pin
+                           SHIFT_REG_RCK_MEAS_L_Pin SHIFT_REG_RCK_STIM_Pin */
+  GPIO_InitStruct.Pin = SHIFT_REG_G_STIM_Pin|SHIFT_REG_CLR_Pin|SHIFT_REG_EN_MEAS_H_Pin|SHIFT_REG_EN_MEAS_L_Pin
+                          |SHIFT_REG_EN_STIM_Pin|SHIFT_REG_G_MEAS_H_Pin|SHIFT_REG_G_MEAS_L_Pin|SHIFT_REG_RCK_MEAS_H_Pin
+                          |SHIFT_REG_RCK_MEAS_L_Pin|SHIFT_REG_RCK_STIM_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PROBE_BTN_INT_Pin */
+  GPIO_InitStruct.Pin = PROBE_BTN_INT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(PROBE_BTN_INT_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : EXT_STIM_1_ON_Pin EXT_STIM_2_ON_Pin EXT_STIM_3_ON_Pin EXT_STIM_4_ON_Pin */
+  GPIO_InitStruct.Pin = EXT_STIM_1_ON_Pin|EXT_STIM_2_ON_Pin|EXT_STIM_3_ON_Pin|EXT_STIM_4_ON_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SHIFT_REG_ERR_Pin */
+  GPIO_InitStruct.Pin = SHIFT_REG_ERR_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(SHIFT_REG_ERR_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
